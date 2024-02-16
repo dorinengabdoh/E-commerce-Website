0a2bf0363f2766415ae09c663b903b90cd6228d3
dorinengabdoh <ngabdohdorine@gmail.com>
2024-02-16 13:19:40 +0100

    ading back End

---
+++ back-end/app.js
@@ -0,0 +1,49 @@
+require('dotenv').config();
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mealsRouter = require('./routes/meals');
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+
+// Enable CORS for all routes
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Replace * with the actual allowed origin
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/meals', mealsRouter);
+
+// catch 404 and forward to error handler/
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // render the error page
+  res.status(err.status || 500);
+  res.send({
+    type: 'error',
+    status: err.status,
+    message: err.message,
+    stack: req.app.get('env') === 'development' ? err.stack : undefined
+  });
+});
+
+module.exports = app;
---
+++ back-end/routes/product.js
@@ -0,0 +1,245 @@
+var express = require("express");
+var router = express.Router();
+let connection = require("../congif/bd_connect");
+
+
+router.get("/", function (req, res, next) {
+  const getquery = ` SELECT
+  product.idProduct,
+  product.namePro,
+  product.imagePro,
+  product.price,
+  category.nameCat,
+  FROM product
+  LEFT JOIN
+    category ON product.idCat = category.idCat
+  ; `;
+  connection.query(getquery, (err, data) => {
+    console.log(data);
+    if (data.length === 0) {
+      let err = new Error(`Meal is not found`);
+      err.status = 404;
+      res.send(err);
+      next(err);
+    } else {
+      res.setHeader("Content-type", "application/json");
+      res.send(data);
+    }
+
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// router.get('/:idRec', function (req, res, next) {
+//   console.log("working");
+//   const idRec = req.params.idRec
+//   console.log("idRec", idRec);
+
+//   const query2 = `
+//   SELECT
+//   recipe.nameRec,
+//   recipe.imageRec,
+//   recipe.areaRec,
+//   category.nameCat,
+//   Instruction.step,
+//   Instruction.nameInstru,
+//   ingredient.nameIngre,
+//   belongTo.quantity,
+//   belongTo.unit
+//   FROM
+//     recipe
+//   LEFT JOIN
+//     category ON recipe.idCat = category.idCat
+//   LEFT JOIN
+//     Instruction ON recipe.idRec = Instruction.idRec
+//   LEFT JOIN
+//     belongTo ON recipe.idRec = belongTo.idRec
+//   LEFT JOIN
+//     ingredient ON belongTo.idIngre = ingredient.idIngre
+//   WHERE
+//   recipe.idRec = ${idRec};`;
+
+//   connection.query(query2, (err, data) => {
+//     console.log(data);
+//     if (err) {
+//       next(err)
+//     } else {
+//       if ((data.length === 0)) {
+//         let error = new Error(`meal with that id not found`)
+//         error.status = 404;
+//         console.log(data);
+//         console.log(error);
+//         return res.status(404).send(error);
+//       } else {
+//         let parseData = data.map(item => ({
+//           ...item,
+//           nameInstru: JSON.parse(item.nameInstru)
+//         }))
+//         res.send(parseData);
+//       }
+//     }
+//   })
+// });
+
+
+
+// router.post("/", function (req, res, next) {
+//   const {
+//     nameRec,
+//     imageRec,
+//     areaRec,
+//     idCat,
+//     nameCat,
+//     idInstru,
+//     nameInstru,
+//     step,
+//     idIngre,
+//     idRec,
+//     nameIngre,
+//     quantity,
+//     unit
+//   } = req.body;
+
+//   console.log("body", req.body);
+
+//   const categoryQuery = `INSERT INTO category (nameCat) VALUES ("beef")`;
+//   const mealQuery = `INSERT INTO recipe (idRec, nameRec, imageRec, areaRec, idCat) values (1,"shawama","https://example.com/spaghetti.jpg", "Spain",1)`;
+//   const ingredientQuery = `insert into ingredient (nameIngre) values ("poulet")`;
+//   const belongToQuery = ` insert  into belongTo (idRec, idIngre, quantity, unit) values (1, 1, 4, "tasse")`;
+//   const instructionsQuery = `INSERT INTO Instruction (idInstru,nameInstru,step)
+//     values (3,
+//       '[{"instruction": "follow steps for shawarma carefully"}]',
+//        '[{"instruction": "follow steps for shawarma carefully"}, {"step": "add tomatoes and"}]'
+//      ),
+//      (4,
+//       '[{"instruction": "follow steps for shawarma carefully"}]',
+//        '[{"instruction": "follow steps for shawarma carefully"}, {"step": "add tomatoes and"}]'
+//      )
+// `;
+
+//   connection.beginTransaction(function (err) {
+//     if (err) {
+//       return next(err);
+//     }
+//     // Insert into mealcategory table
+//     connection.query(categoryQuery, [idCat, nameCat], function (error, results) {
+//       if (error) {
+//         return connection.rollback(function () {
+//           next(error);
+//         });
+//       }
+//       // Insert into meals table
+//       connection.query(mealQuery, [nameRec, imageRec, areaRec, idCat], function (error, results) {
+//         if (error) {
+//           return connection.rollback(function () {
+//             next(error);
+//           });
+//         }
+//         const mealIdFromMealsInsert = results.insertId;
+//         // Insert into mealInstructions table
+//         connection.query(instructionsQuery, [idInstru, nameInstru, step], function (error, results) {
+//           console.log("instructions querry ", results);
+//           if (error) {
+//             return connection.rollback(function () {
+//               next(error);
+//             });
+//           }
+
+//           connection.query(ingredientQuery, [nameIngre], function (error, results) {
+//             console.log(" ingredientquerry ", results);
+//             if (error) {
+//               return connection.rollback(function () {
+//                 next(error)
+//               })
+//             }
+//             connection.query(belongToQuery, [idRec, idIngre, quantity,unit], function (error, results) {
+//               console.log(" belongToquerry ", results);
+//               if (error) {
+//                 return connection.rollback(function () {
+//                   next(error)
+//                 })
+//               }
+//               connection.commit(function (err) {
+//                 if (err) {
+//                   return connection.rollback(function () {
+//                     next(err);
+//                   });
+//                 }
+//                 console.log('Transaction Complete.');
+//                 res.setHeader("Content-type", "application/json")
+//                 res.status(201).send(results);
+//               });
+//             })
+//           });
+//         });
+//       });
+//     });
+//   });
+// });
+
+// // post du router
+// router.post("/", function (req, res, next) {
+//   const recipeQuery = `INSERT INTO recipe (nameRec, imageRec, areaRec, idCat)
+//   VALUES ('Gâteau a la banane', 'https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg', 'France', 1);`
+//   const ingreQuery = `INSERT INTO Ingredient (nameIngre)
+//   VALUES ('Farine');`
+//   const instruQuery = `INSERT INTO instruction (nameInstru, step, idRec)
+//   VALUES ('Mélanger la farine, le chocolat et les autres ingrédients dans un bol.', 2, 1);`
+//   const catQuery = `INSERT INTO category (nameCat)
+//   VALUES ('Desserts');`
+//   const belongToQuery = `INSERT INTO belongTo (idIngre, idRec, quantity, unit)
+//   VALUES (2, 1, 6, 'tasse');`
+//   connection.query(recipeQuery, ingreQuery[instruQuery], catQuery, belongToQuery, (err, data) => {
+//     if (err) next(err);
+//     else res.status(201).send({ id: data.insertId });
+//   });
+// });
+
+// // post  
+// router.put("/:idRec", function (req, res, next) {
+//   const idRec = req.params.idRec;
+//   console.log(req.body);
+//   const createQuery = `update recipe set nameRec = "gateau simpe" where idRec=${idRec}`;
+//   connection.query(createQuery, (err, data) => {
+//     if (err) next(err);
+//     else res.status(201).send(data);
+//   });
+// });
+
+// // delete
+
+// router.delete("/:idRec", function (req, res, next) {
+//   const idRec = req.params.idRec;
+//   console.log(req.body);
+//   const createQuery = `DELETE m FROM recipe m INNER JOIN Instruction i ON m.idRec = i.idRec INNER JOIN belongTo
+//   mi ON m.idRec = mi.idRec INNER JOIN ingredient ing ON mi.idIngre = ing.idIngre WHERE m.idRec = ${idRec};`;
+//   connection.query(createQuery, (err, data) => {
+//     if (err) {
+//       console.log(err);
+//       if (err.message === "not found") next();
+//       else {
+//         next();
+//       }
+//     } else res.send(data);
+//   });
+// });
+
+module.exports = router;
